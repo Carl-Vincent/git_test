042dec4e86a549235812f5b2b575c51fcbc549f1
Bryan <782784691@qq.com>
2022-03-29 22:04:47 +0800

    third commit

--- src/test/java/com/example/git/GitTestApplicationTests.java
+++ src/test/java/com/example/git/GitTestApplicationTests.java
@@ -13,6 +13,7 @@ class GitTestApplicationTests {
 	public static void main(String[] args) {
 		System.out.println("hello, git!");
 		System.out.println("hello, github!");
+		System.out.println("hello, gitee!");
 	}
 
 }
