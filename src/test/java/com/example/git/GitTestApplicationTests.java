461b6e2d617715dc668964aae3a9fb4533720f96
Bryan <782784691@qq.com>
2022-03-29 22:02:06 +0800

    first commit

---
+++ src/test/java/com/example/git/GitTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.git;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello, git!");
+	}
+
+}
